0ee6f267b3e65239c86a7367df8b09978b7a4b10
K@zuki <k@zuki.dev>
2023-04-02 03:18:45 +0900

    add: setup google anlytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,16 @@ export default function Document() {
       <Body>
         <Main />
         <NextScript />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZJ7C2QM9W7"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ZJ7C2QM9W7');
+          `
+        }}></script>
       </Body>
     </Html>
   )
