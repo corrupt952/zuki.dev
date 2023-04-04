b464544f746a4bea801d475cdb3f650119849e3a
K@zuki <k@zuki.dev>
2023-04-04 23:19:12 +0900

    change: use Script element for google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,31 @@
-import { styled } from '@mui/material'
-import { Html, Head, Main, NextScript } from 'next/document'
+import { styled } from "@mui/material";
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
-const Body = styled('body')({
-  overflowY: 'scroll',
-})
+const Body = styled("body")({
+  overflowY: "scroll",
+});
+
+export const GoogleAnalytics = () => {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZJ7C2QM9W7"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ZJ7C2QM9W7');
+          `}
+      </Script>
+    </>
+  );
+};
 
 export default function Document() {
   return (
@@ -12,17 +34,7 @@ export default function Document() {
       <Body>
         <Main />
         <NextScript />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZJ7C2QM9W7"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-ZJ7C2QM9W7');
-          `
-        }}></script>
       </Body>
     </Html>
-  )
+  );
 }
