47bfcc096b126ee9fff9f992ebf07027c61c47a6
K@zuki <k@zuki.dev>
2023-04-04 23:48:17 +0900

    change: use any type to avoid point

--- src/components/Elements/LinkText.tsx
+++ src/components/Elements/LinkText.tsx
@@ -1,6 +1,6 @@
 import { Config } from "@/config";
 import { DEFAULT_LOCALE, I18nContext } from "@/libs/i18n";
-import { styled } from "@mui/material";
+import { styled, LinkProps } from "@mui/material";
 import Link from "next/link";
 import { forwardRef, useContext } from "react";
 
@@ -15,15 +15,19 @@ const linkOptions = {
 
 const StyledInnerLink = styled(Link)(linkOptions);
 
-export const LinkText = forwardRef((props: any, ref) => {
+// TODO: anyを何とかする
+export const LinkText = forwardRef<HTMLAnchorElement, any>(function _LinkText(
+  props,
+  ref
+) {
   const { locale } = useContext(I18nContext);
   const { href, ...linkProps } = props;
-  let uri = href;
+  let uri = href || "/";
   let target = "_self";
   let rel = "noopener noreferrer";
 
   if (uri.startsWith("/")) {
-    uri = locale === DEFAULT_LOCALE ? href : `/${locale}${href}`;
+    uri = locale === DEFAULT_LOCALE ? uri : `/${locale}${uri}`;
   } else {
     target = "_blank";
   }
