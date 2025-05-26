97d7ba13716d95df90a25ca7cb372829cf467d30
K@zuki <k@zuki.dev>
2025-05-27 00:11:54 +0900

    chore: remove lg:grid-cols-4 in portofolio

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -186,7 +186,7 @@ export default function Portfolio() {
   return (
     <>
       <Heading>{t('title')}</Heading>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {projects.map((project) => (
           <ProjectCard key={project.title} project={project} />
         ))}
